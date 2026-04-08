c9c993d50ba47a90f4631e23142171c82cf8f8e1
Ilay Gural <ilaygural@gmail.com>
2026-04-08 18:20:05 +0700

    feat: replace registration function with UserCreationForm + CreateView

--- users/views.py
+++ users/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic.edit import CreateView
 
 from users.forms import LoginUserForm, RegisterUserForm
 
@@ -32,14 +33,21 @@ class LoginUser(LoginView):
 #     logout(request)
 #     return HttpResponseRedirect(reverse('users:login'))
 
-def register(request):
-    if request.method == "POST":
-        form = RegisterUserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)  # создание объекта без сохранения в БД
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            return render(request, 'users/register_done.html')
-    else:
-        form = RegisterUserForm()
-    return render(request, 'users/register.html', {'form': form})
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
+    extra_context = {'title': 'Регистрция'}
+
+
+# def register(request):
+#     if request.method == "POST":
+#         form = RegisterUserForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)  # создание объекта без сохранения в БД
+#             user.set_password(form.cleaned_data['password'])
+#             user.save()
+#             return render(request, 'users/register_done.html')
+#     else:
+#         form = RegisterUserForm()
+#     return render(request, 'users/register.html', {'form': form})
